d24ab8e19510ad1c7ee4b992f14c07df9fbf5340
Derricknjaramba <njarambaderrick@gmail.com>
2024-07-07 18:55:03 +0300

    arrayfindmethod

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+let superBowls = [
+    { year: 1999, result: 'L' },
+    { year: 2000, result: 'W' },
+    { year: 2001, result: 'L' },
+    
+  ];
+  
+  
+  function superbowlWin(superBowlsArray) {
+    let winningYearObj = superBowlsArray.find(superBowl => superBowl.result === 'W');
+    return (winningYearObj ? winningYearObj.year : undefined);
+  }
+  
+  
+  let firstWinYear = superbowlWin(superBowls);
+  console.log(firstWinYear);
